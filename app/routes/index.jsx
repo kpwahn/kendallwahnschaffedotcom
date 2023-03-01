38d7ccbb1761c8008b06c5363ff350f8b3c814b4
kpwahn <kpwahnschaffe@gmail.com>
2023-03-01 20:27:10 +0000

    more tweaking

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -15,8 +15,10 @@ export default function Index() {
 
         if (scrollTop > 100 && scrollTop < 200) {
           headerRef.current.classList.replace('rounded-b-[40%]', 'rounded-b-[20%]')
+          headerRef.current.classList.replace('pb-36', 'pb-16')
         } else if (scrollTop > 200) {
           headerRef.current.classList.replace('rounded-b-[20%]', 'rounded-b-0')
+          headerRef.current.classList.replace('pb-16', 'pb-4')
         }
 
       } else if (oldScrollTop > scrollTop) {
@@ -24,8 +26,10 @@ export default function Index() {
         // Scrolling Up
         if (scrollTop < 100) {
           headerRef.current.classList.replace('rounded-b-[20%]', 'rounded-b-[40%]')
+          headerRef.current.classList.replace('pb-16', 'pb-36')
         } else if (scrollTop < 200) {
           headerRef.current.classList.replace('rounded-b-0', 'rounded-b-[20%]')
+          headerRef.current.classList.replace('pb-4', 'pb-16')
         }
       }
 
@@ -35,7 +39,7 @@ export default function Index() {
 
   return (
     <main className="h-[200%]">
-      <header ref={headerRef} className="text-3xl pt-4 md:pt-6 text-white flex justify-center h-96 rounded-b-[40%] bg-blue-400 transition-all duration-1000">
+      <header ref={headerRef} className="sticky -top-80 text-3xl pt-4 md:pt-6 text-white flex justify-center items-end h-96 rounded-b-[40%] pb-36 bg-blue-400 transition-all duration-[800ms]">
         Kendall Wahnschaffe
       </header>
       
